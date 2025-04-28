980625fee9ddd27df9b117eac6639012d5e0850e
Wall3n <franmoreno1506@gmail.com>
2025-04-28 11:46:23 +0200

    Refac: Reorganizacion en un mismo archivo para la subida a mooshak

--- A_AR/avance-rapido.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __AVANCE_RAPIDO__
-#define __AVANCE_RAPIDO__
-
-#include <vector>
-
-std::vector<int> avance_rapido(std::vector<int> C, const std::vector<std::vector<int>> &b, const std::vector<int> &c, int nw, int nt);
-
-int objetivo(const std::vector<int> &S, const std::vector<std::vector<int>> &b);
-#endif
